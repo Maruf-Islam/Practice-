4d9a7aee7962b6a8643c10f4fd2af083198ef486
Aminulislam1 <73760592+Maruf-Islam@users.noreply.github.com>
2021-06-27 21:15:06 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) {
+	printf("    ^ \n");
+	printf("   /|\n");
+	printf("  / |\n");
+	printf(" /  |\n");
+	printf("/___|\n");
+	printf("This is my first C Program \n");
+	return 0;
+}
+
+
